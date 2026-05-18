750905b3c8811cf68f71566ab5b9b460ede79739
Nattanon Vinyangkoon <avinyangkoon@gmail.com>
2026-05-18 14:15:55 +0700

    Fix upgrade by deactivating legacy res.users views

--- view_auto_fields/__manifest__.py
+++ view_auto_fields/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "15.0.1.1.0",
     "depends": ["base", "web"],
     "data": [
+        "views/res_users_cleanup_views.xml",
         "security/view_auto_fields_security.xml",
         "data/ir_config_parameter.xml",
         "views/res_config_settings_views.xml",
